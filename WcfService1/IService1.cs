13be4d6d4547288500886db7d246331ceda500bd
Luiz Carlos Flores dos Santos Junior <luiz_neobit@hotmail.com>
2017-09-14 22:07:24 -0300

    Initial commit

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+
+    [ServiceContract(Namespace = "http://www.portocred.com.br")]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(Parametro param);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Adicione suas operações de serviço aqui
+    }
+
+    [DataContract(Namespace = "http://www.portocred.com.br")]
+    public class Parametro
+    {
+        public int cod { get; set; }
+        public string msg { get; set; }
+    }
+
+    // Use um contrato de dados como ilustrado no exemplo abaixo para adicionar tipos compostos a operações de serviço.
+    [DataContract(Namespace = "http://www.portocred.com.br")]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Services;
+
+namespace WcfService1
+{
+    public class Service1 : IService1
+    {
+        public string GetData(Parametro param)
+        {
+            return string.Format("You entered: {0}", param.msg);
+        }
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
